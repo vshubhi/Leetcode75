86e195894ae4af6600224fa8e7696f810443fe4a
Shubhi Verma <shubhi_verma@epam.com>
2023-09-23 12:45:52 +0530

    LinkedList

---
+++ Linked List/Medium/maximum_twin_sum_linked_list/python/Solution.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next==None:
+          head = None
+          return head
+        slow= ListNode()
+        slow.next = head
+        fast = head
+        while fast and fast.next!=None:
+          slow = slow.next
+          fast = fast.next.next
+        temp = slow.next
+        slow.next = temp.next
+        return head
